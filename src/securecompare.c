2535004d3f92ee671fe4ba15e74f788dc057f5e3
Edy <edysanchez@gmail.com>
2013-04-19 00:20:09 +0200

    add first and basic version

--- src/securecompare.c
+++ src/securecompare.c
@@ -1,10 +1,18 @@
 #include<stdio.h>
 int securecompare(char *,char *);
-int main() {
-		printf("hola\n");
-		printf("%d",1^1);
-		securecompare(NULL,NULL);
+int main(int argc,char *argv[]) {
+		printf("secure compare\n");
+		printf("%d",1==1);
+		securecompare("1123","21");
 }
 int securecompare(char *inputValue,char * referenceValue) {
+		int index=0;
+		int res=0;
+		while(*(inputValue+index)!='\0') {
+				printf("INDEX\n");
+				index++;
+				printf("res %d",*(inputValue+index)^*(referenceValue+index));
+				return *(inputValue+index)^*(referenceValue+index);
+		}
 		return 1;
 }
