c68e11ed402632d735c4e39d1138c3f997c07f33
Edgar Sanchez Cortés <esanchez@applus-white.(none)>
2013-04-19 16:41:47 +0200

    basic secure compare with XOR

--- src/securecompare.c
+++ src/securecompare.c
@@ -1,18 +1,23 @@
 #include<stdio.h>
-int securecompare(char *,char *);
+
+#define DEFAULT_NUMBER_ARG 3
+#define KEY_LEN 2
+
+int securecompare(char *,char *,int len);
+
 int main(int argc,char *argv[]) {
-		printf("secure compare\n");
-		printf("%d",1==1);
-		securecompare("1123","21");
+    printf("secure compare\n");
+    printf("arguments number: %d\n",argc);
+    if(argc==DEFAULT_NUMBER_ARG) {
+	printf("res= %d\n",securecompare(argv[1],argv[2],KEY_LEN));
+    }
 }
-int securecompare(char *inputValue,char * referenceValue) {
-		int index=0;
-		int res=0;
-		while(*(inputValue+index)!='\0') {
-				printf("INDEX\n");
-				index++;
-				printf("res %d",*(inputValue+index)^*(referenceValue+index));
-				return *(inputValue+index)^*(referenceValue+index);
-		}
-		return 1;
+
+int securecompare(char *inputValue,char * referenceValue, int len) {
+    int index=0;
+    int res=0;
+    for(index=0;index<len;index++) {
+	res^=*(inputValue+index)^*(referenceValue+index);
+    }
+    return res;
 }
